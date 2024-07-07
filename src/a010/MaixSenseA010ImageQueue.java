8be2fc6077040713759ecbee62449b62877eda9d
PBernalPolo <pablo.bernal.polo@gmail.com>
2024-07-07 13:30:43 +0200

    refactor: updates method name to comply with previous commit
    We also improve documentation.

--- src/a010/MaixSenseA010ImageQueue.java
+++ src/a010/MaixSenseA010ImageQueue.java
@@ -8,14 +8,31 @@ import java.util.Queue;
 
 
 
+/**
+ * Implements a queue to which {@link MaixSenseA010Image}s are added and consumed by {@link MaixSenseA010ImageConsumer}s in parallel.
+ * <p>
+ * The producer-consumer design pattern is used.
+ */
 public class MaixSenseA010ImageQueue
     implements Runnable
 {
     ////////////////////////////////////////////////////////////////
     // PRIVATE VARIABLES
     ////////////////////////////////////////////////////////////////
+    
+    /**
+     * Queue to which {@link MaixSenseA010Image}s are added, and consumed by {@link MaixSenseA010ImageConsumer}s.
+     */
     private Queue<MaixSenseA010Image> queue;
+    
+    /**
+     * List of {@link MaixSenseA010ImageConsumer}s that consume the {@link MaixSenseA010Image}s.
+     */
     private List<MaixSenseA010ImageConsumer> listeners;
+    
+    /**
+     * True if the thread that consumes the queue must be kept running.
+     */
     private boolean keepRunning;
     
     
@@ -24,6 +41,9 @@ public class MaixSenseA010ImageQueue
     // PUBLIC CONSTRUCTORS
     ////////////////////////////////////////////////////////////////
     
+    /**
+     * Constructs a {@link MaixSenseA010ImageQueue}.
+     */
     public MaixSenseA010ImageQueue()
     {
         this.queue = new ArrayDeque<MaixSenseA010Image>();
@@ -37,18 +57,33 @@ public class MaixSenseA010ImageQueue
     // PUBLIC METHODS
     ////////////////////////////////////////////////////////////////
     
+    /**
+     * Adds a {@link MaixSenseA010ImageConsumer} to the list of {@link MaixSenseA010Image} consumers.
+     * 
+     * @param consumer  {@link MaixSenseA010ImageConsumer} to be added.
+     */
     public void addListener( MaixSenseA010ImageConsumer consumer )
     {
         this.listeners.add( consumer );
     }
     
     
+    /**
+     * Removes a {@link MaixSenseA010ImageConsumer} from the list of {@link MaixSenseA010Image} consumers.
+     * 
+     * @param consumer  {@link MaixSenseA010ImageConsumer} to be removed.
+     */
     public void removeListener( MaixSenseA010ImageConsumer consumer )
     {
         this.listeners.remove( consumer );
     }
     
     
+    /**
+     * Adds a new {@link MaixSenseA010Image} to the queue.
+     * 
+     * @param image     {@link MaixSenseA010Image} to be added to the queue.
+     */
     public void add( MaixSenseA010Image image )
     {
         this.queue.add( image );
@@ -58,6 +93,9 @@ public class MaixSenseA010ImageQueue
     }
     
     
+    /**
+     * Starts a thread in which the {@link MaixSenseA010Image}s will be consumed by the {@link MaixSenseA010ImageConsumer}s.
+     */
     public synchronized void start()
     {
         if( !this.keepRunning ) {
@@ -70,19 +108,25 @@ public class MaixSenseA010ImageQueue
     }
     
     
+    /**
+     * Stops the thread in which the {@link MaixSenseA010Image}s are consumed by the {@link MaixSenseA010ImageConsumer}s.
+     */
     public synchronized void stop()
     {
         this.keepRunning = false;
     }
     
     
+    /**
+     * Runs in a {@link Thread}, polling the {@link MaixSenseA010Image}s and consuming them by the {@link MaixSenseA010ImageConsumer}s.
+     */
     public void run()
     {
         while( this.keepRunning ) {
             while( !this.queue.isEmpty() ) {
                 MaixSenseA010Image image = this.queue.poll();
                 for( MaixSenseA010ImageConsumer consumer : this.listeners ) {
-                    consumer.onImageQueued( image );
+                    consumer.consumeImage( image );
                 }
             }
             try {
